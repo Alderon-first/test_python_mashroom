45c9bc369ca2c1af1248c88cc804f20ff2ecb4a2
eushko <eushko@pikemedia.ru>
2022-12-19 00:50:27 +0300

    +model/pages/...res_1!!!!!

--- qa_guru_python_3_7/utils/resouce_1.py
+++ qa_guru_python_3_7/utils/resouce_1.py
@@ -5,9 +5,4 @@ def path_file(name_file):
     return os.path.abspath(
         os.path.join(os.path.dirname(__file__),  os.path.pardir, os.path.pardir,  name_file))
 
-print(os.path.abspath(
-        os.path.join(os.path.dirname(__file__),  os.path.pardir, os.path.pardir, "resource/текст_1920-1080.jpg")))
-print(os.path.join(os.path.dirname(__file__),  os.path.pardir,  "resource/текст_1920-1080.jpg"))
-print(os.path.join(os.path.dirname(__file__),  "resource/текст_1920-1080.jpg"))
-
 
