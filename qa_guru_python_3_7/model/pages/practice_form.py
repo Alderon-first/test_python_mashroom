268b8a9fd978d137a3e8fc15452d4e3c43ba950f
eushko <eushko@pikemedia.ru>
2022-12-18 22:56:13 +0300

    +model/pages/...check_info

--- qa_guru_python_3_7/model/pages/practice_form.py
+++ qa_guru_python_3_7/model/pages/practice_form.py
@@ -4,6 +4,7 @@ from selene.support.shared import browser
 import tests_demoqa.resouce
 
 
+# заполнение формы
 def form_data():
     browser.element('[id="firstName"]').should(be.blank).type('Имя')
     browser.element('[id="lastName"]').should(be.blank).type('Отчество')
@@ -13,7 +14,7 @@ def form_data():
     browser.element('[id="userNumber"]').should(be.blank).type('1234567890')
     # загрузка картинки
     browser.element('#uploadPicture').set_value(
-        tests_demoqa.resouce.path_file('resourses/текст_1920-1080.jpg'))
+        tests_demoqa.resouce.path_file('resource/текст_1920-1080.jpg'))
     # календарь
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__year-select [value="1990"]').click()
@@ -27,3 +28,17 @@ def form_data():
     browser.element('#react-select-3-input').type('NCR').press_enter()
     browser.element('#react-select-4-input').type('Delhi').press_enter()
     browser.element('#currentAddress').type('currentAddress')
+
+
+def check_info():
+    # проверки
+    browser.all('.table-responsive').all('tr').element(1).should(have.text('Имя Отчество'))
+    browser.all('.table-responsive').all('tr').element(2).should(have.text('test@test.ru'))
+    browser.all('.table-responsive').all('tr').element(3).should(have.text('Female'))
+    browser.all('.table-responsive').all('tr').element(4).should(have.text('1234567890'))
+    browser.all('.table-responsive').all('tr').element(5).should(have.text('02 December,1990'))
+    browser.all('.table-responsive').all('tr').element(6).should(have.text('Arts'))
+    browser.all('.table-responsive').all('tr').element(7).should(have.text('Sports'))
+    browser.all('.table-responsive').all('tr').element(8).should(have.text('текст_1920-1080.jpg'))
+    browser.all('.table-responsive').all('tr').element(9).should(have.text('currentAddress'))
+    browser.all('.table-responsive').all('tr').element(10).should(have.text('NCR Delhi'))
--- tests_demoqa/test_form.py
+++ tests_demoqa/test_form.py
@@ -1,7 +1,7 @@
 from selene.support.shared import browser
-from selene import have
 
 from qa_guru_python_3_7.model.pages import practice_form
+from qa_guru_python_3_7.model.pages.practice_form import check_info
 
 
 def test_practice_form():
@@ -16,16 +16,8 @@ def test_practice_form():
     # ожидание 2 секунды, иначе тест не видл формы и падал
     #time.sleep(2)
 
-    # проверки
-    browser.all('.table-responsive').all('tr').element(1).should(have.text('Имя Отчество'))
-    browser.all('.table-responsive').all('tr').element(2).should(have.text('test@test.ru'))
-    browser.all('.table-responsive').all('tr').element(3).should(have.text('Female'))
-    browser.all('.table-responsive').all('tr').element(4).should(have.text('1234567890'))
-    browser.all('.table-responsive').all('tr').element(5).should(have.text('02 December,1990'))
-    browser.all('.table-responsive').all('tr').element(6).should(have.text('Arts'))
-    browser.all('.table-responsive').all('tr').element(7).should(have.text('Sports'))
-    browser.all('.table-responsive').all('tr').element(8).should(have.text('текст_1920-1080.jpg'))
-    browser.all('.table-responsive').all('tr').element(9).should(have.text('currentAddress'))
-    browser.all('.table-responsive').all('tr').element(10).should(have.text('NCR Delhi'))
+    check_info()
     # закрыть форму
     browser.element('#closeLargeModal').press_enter()
+
+
