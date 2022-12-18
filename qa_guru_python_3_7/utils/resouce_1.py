979ddda3bdff63d343a131fe1eaed9a73ed9b5b9
eushko <eushko@pikemedia.ru>
2022-12-19 00:53:11 +0300

    +model/pages/...res_1 подсказки

--- qa_guru_python_3_7/utils/resouce_1.py
+++ qa_guru_python_3_7/utils/resouce_1.py
@@ -1,9 +1,9 @@
 import os
 
+
 # узнать относительный путь к файлу
 def path_file(name_file):
     return os.path.abspath(
-        os.path.join(os.path.dirname(__file__), # узнать абсолютный путь к файлу
-                     os.path.pardir, os.path.pardir,  name_file)) # обрезать его до относительного, где os.path.pardir шаг вверх
-
-
+        os.path.join(os.path.dirname(__file__),  # узнать абсолютный путь к файлу
+                     os.path.pardir, os.path.pardir, name_file))  # обрезать его до относительного,
+    # где os.path.pardir шаг вверх
