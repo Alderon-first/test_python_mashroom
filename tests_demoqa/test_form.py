5ba66cfc048701aaa57558e41cf4fd1a300b4424
eushko <eushko@pikemedia.ru>
2022-12-27 21:13:42 +0300

    + указание модуля

--- tests_demoqa/test_form.py
+++ tests_demoqa/test_form.py
@@ -3,19 +3,19 @@ from qa_guru_python_3_7.model.pages.practice_form import check_info, close_form,
 
 
 def test_practice_form():
-    open_page_practice_form()
+    practice_form.open_page_practice_form()
 
     # заполнение формы
     practice_form.form_data()
 
     # отправка формы (нажать на кнопку, проскролив до нее)
-    send_form()
+    practice_form.send_form()
 
     # ожидание 2 секунды, иначе тест не видл формы и падал
     # time.sleep(2)
 
-    check_info()
+    practice_form.check_info()
     # закрыть форму
-    close_form()
+    practice_form.close_form()
 
 
