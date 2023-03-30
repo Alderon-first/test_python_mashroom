1bf514ce069f298e6083811886d13782a77292b1
eushko <eushko@pikemedia.ru>
2023-03-31 02:12:23 +0300

    + смешанные тесты api+Ui для админки + вкладки

--- tests/API/test_organizer_pages_check.py
+++ tests/API/test_organizer_pages_check.py
@@ -1,3 +1,5 @@
+import os
+
 from dotenv import load_dotenv
 from selene import have
 load_dotenv()
@@ -7,12 +9,21 @@ def test_activity_page(mashroom_api):
     mashroom_api.open('')
     mashroom_api.element('//*[@id="app"]/div[1]/main/div/div/div/div/h1').\
         should(have.text('Мои мероприятия'))
-    mashroom_api.element('#app > div.v-application--wrap > main > div > div > div > div > div.row.mb-4.no-gutters > div.col-12.col-md.mr-6.col > div > div > div.v-slide-group__wrapper > div > div.v-tab.v-tab--active').\
+    mashroom_api.element(
+        '#app > div.v-application--wrap > main > div > div > div > div > div.row.mb-4.no-gutters > '
+        'div.col-12.col-md.mr-6.col > div > div > div.v-slide-group__wrapper > div > div.v-tab.v-tab--active').\
         should(have.text('ТЕКУЩИЕ И ЗАПЛАНИРОВАННЫЕ'))
-    mashroom_api.element('#app > div.v-application--wrap > main > div > div > div > div > div.row.mb-4.no-gutters > div.col-12.col-md.mr-6.col > div > div > div.v-slide-group__wrapper > div > div:nth-child(3)').\
+    mashroom_api.element(
+        '#app > div.v-application--wrap > main > div > div > div > div > div.row.mb-4.no-gutters > '
+        'div.col-12.col-md.mr-6.col > div > div > div.v-slide-group__wrapper > div > div:nth-child(3)').\
         should(have.text('АРХИВНЫЕ'))
-    mashroom_api.element('#app > div.v-application--wrap > main > div > div > div > div > div.row.mb-4.no-gutters > div.col.col-auto > button > span'). \
+    mashroom_api.element(
+        '#app > div.v-application--wrap > main > div > div > div > div > div.row.mb-4.no-gutters > '
+        'div.col.col-auto > button > span'). \
         should(have.text('НОВОЕ МЕРОПРИЯТИЕ'))
+    mashroom_api.element(
+        '#app > div.v-application--wrap > div:nth-child(1) > header > div > div.d-flex.align-center > span'). \
+        should(have.text(os.getenv("LOGIN")))
 
 
 def test_profile_page(mashroom_api):
@@ -23,12 +34,16 @@ def test_profile_page(mashroom_api):
 
 def test_profile_page_tab_arch(mashroom_api):
     mashroom_api.open('https://event-admin-demo.pikemedia.live/?tab=1')
-    mashroom_api.element('#app > div.v-application--wrap > main > div > div > div > div > div.row.mb-4.no-gutters > div.col-12.col-md.mr-6.col > div > div > div.v-slide-group__wrapper > div > div.v-tab.v-tab--active').\
+    mashroom_api.element('#app > div.v-application--wrap > main > div > div > div > div > div.row.mb-4.no-gutters > '
+                         'div.col-12.col-md.mr-6.col > div > div > div.v-slide-group__wrapper > div > '
+                         'div.v-tab.v-tab--active').\
         should(have.attribute('aria-selected', "true"))
 
 
 def test_profile_page_tab_live(mashroom_api):
     mashroom_api.open('https://event-admin-demo.pikemedia.live/?tab=0')
-    mashroom_api.element('#app > div.v-application--wrap > main > div > div > div > div > div.row.mb-4.no-gutters > div.col-12.col-md.mr-6.col > div > div > div.v-slide-group__wrapper > div > div.v-tab.v-tab--active').\
+    mashroom_api.element('#app > div.v-application--wrap > main > div > div > div > div > div.row.mb-4.no-gutters > '
+                         'div.col-12.col-md.mr-6.col > div > div > div.v-slide-group__wrapper > div > '
+                         'div.v-tab.v-tab--active').\
         should(have.attribute('aria-selected', "true"))
 
