df829176d25b9b022493eb34476612c6f860a0ba
eushko <eushko@pikemedia.ru>
2023-03-30 19:53:28 +0300

    healthcheck update

--- tests/API/service_healthcheck.py
+++ tests/API/service_healthcheck.py
@@ -1,29 +1,25 @@
 from pytest_voluptuous import S
-
+import json
 from test_python_mashroom.API.schemas.user import create_user_schema
 
 
 def test_viewer_healthcheck(example_api):
     response = example_api.get("/viewer/health")
     assert response.status_code == 200
-    assert True == response.json().get("ок")
-    '''
-        тут json сождержит "ok": true - тоже не понятно, как проверить
-    '''
+    response_body = response.json()
+    assert response_body['ok'] == True
 
 
 def test_chat_healthcheck(example_api):
-    response = example_api.get("chat/healthcheck")
+    response = example_api.get("/chat/healthcheck")
     assert response.status_code == 200
-    assert "true" == response.json()
-    '''
-    тут json сождержит только  true без заголовка - не понимаю, как провреить
-    '''
+    deserialized = json.loads(response.content)
+    print(deserialized)
+    assert deserialized is True
 
 
 def test_eventor_healthcheck(example_api):
     response = example_api.get("/eventor/health")
     assert response.status_code == 200
-    assert "OK" == response.json().get("DB")
-    assert "OK" == response.json().get("eventor")
-
+    assert "OK" == response.json().get("DB"), "Значение DB!=OK"
+    assert "OK" == response.json().get("eventor"), "eventor DB!=OK"
