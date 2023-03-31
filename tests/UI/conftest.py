3af9a0aeedd31ea697a80971566e913104a8e4ff
eushko <eushko@pikemedia.ru>
2023-03-31 05:49:22 +0300

    -раскоментировала подключение к селениуму

--- tests/UI/conftest.py
+++ tests/UI/conftest.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 @pytest.fixture(scope="session", autouse=True)
 def browser_user_site():
-    '''
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -24,7 +24,7 @@ def browser_user_site():
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options)
     browser.config.driver = driver
-    '''
+
     browser.config.base_url = os.getenv("STEND_URL_USER_UI")
     browser.config.window_width = 1920
     browser.config.window_height = 1080
