9e4bb46aaae917c92add79fdb2e9b72049a3cbb2
eushko <eushko@pikemedia.ru>
2023-03-31 03:06:12 +0300

    + test_activity_CRUD.py

---
+++ tests/API/test_activity_CRUD.py
@@ -0,0 +1,25 @@
+from selene import have
+
+
+def test_profile_page_createactivity(mashroom_api):
+    mashroom_api.open('')
+    mashroom_api.element(
+        '#app > div.v-application--wrap > main > div > div > div > div > div.row.mb-4.no-gutters > '
+        'div.col.col-auto > button > span').click()
+    mashroom_api.element("//div[@id='app']/div[3]/div/div/div[2]/span/form/div/div/span/div/div/div/div").click()
+    mashroom_api.element("//div[@id='app']/div[3]/div/div/div[2]/span/form/div/div/span/div/div/div/div/input").\
+        type("test_activity_py")
+    mashroom_api.element("//div[@id='app']/div[3]/div/div/div[2]/span/form/button/span").click()
+    mashroom_api.element(
+        '//*[@id="app"]/div/main/div/div/div/div/div/div[1]/span[1]'). \
+        should(have.text('test_activity_py'))
+
+
+def test_profile_page_deleteactivity(mashroom_api):
+    mashroom_api.open('')
+    mashroom_api.element(
+        '//*[@id="app"]/div[1]/main/div/div/div/div/div[2]/div/div/div/div/div[1]/table/tbody/tr/td[5]/button[3]').click()
+    mashroom_api.element('#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > button:nth-child(3)').click()
+    mashroom_api.element(
+        '//*[@id="app"]/div/main/div/div/div/div/div[2]/div/div/div/div/div[1]/table/tbody/tr/td/div/div'). \
+        should(have.text('Нажмите на кнопку "Новое мероприятие", чтобы создать мероприятие'))
--- tests/API/test_organizer_pages_check.py
+++ tests/API/test_organizer_pages_check.py
@@ -1,10 +1,9 @@
 import os
 
 from dotenv import load_dotenv
-from selene import have, be
+from selene import have
+
 load_dotenv()
-import time
-from test_python_mashroom.API.utils.base_session import BaseSession
 
 
 def test_activity_page(mashroom_api):
@@ -52,27 +51,4 @@ def test_profile_page_tab_live(mashroom_api):
                          'div.v-tab.v-tab--active').\
         should(have.attribute('aria-selected', "true"))
 
-
-def test_profile_page_createactivity(mashroom_api):
-    mashroom_api.open('')
-    mashroom_api.element(
-        '#app > div.v-application--wrap > main > div > div > div > div > div.row.mb-4.no-gutters > '
-        'div.col.col-auto > button > span').click()
-    mashroom_api.element("//div[@id='app']/div[3]/div/div/div[2]/span/form/div/div/span/div/div/div/div").click()
-    mashroom_api.element("//div[@id='app']/div[3]/div/div/div[2]/span/form/div/div/span/div/div/div/div/input").\
-        type("test_activity_py")
-    mashroom_api.element("//div[@id='app']/div[3]/div/div/div[2]/span/form/button/span").click()
-    mashroom_api.element(
-        '//*[@id="app"]/div/main/div/div/div/div/div/div[1]/span[1]'). \
-        should(have.text('test_activity_py'))
-
-
-def test_profile_page_deleteactivity(mashroom_api):
-    mashroom_api.open('')
-    mashroom_api.element(
-        '//*[@id="app"]/div[1]/main/div/div/div/div/div[2]/div/div/div/div/div[1]/table/tbody/tr/td[5]/button[3]').click()
-    mashroom_api.element('#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > button:nth-child(3)').click()
-    mashroom_api.element(
-        '//*[@id="app"]/div/main/div/div/div/div/div[2]/div/div/div/div/div[1]/table/tbody/tr/td/div/div'). \
-        should(have.text('Нажмите на кнопку "Новое мероприятие", чтобы создать мероприятие'))
 
